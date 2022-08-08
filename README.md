53406712ebd8e159b28fe29b664a54e076e1efa3
Kostyantyn Skaklinyov <qastarway@gmail.com>
2022-08-08 22:07:22 +0300

    Commit message 1

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Create a single test using WD: navigate to the Google search page,
 search for an image, and verify that an image tab contains images.
 Add your home task to Git and share the link here.
+
+OK!
--- src/test/java/HelloWebDriver.java
+++ src/test/java/HelloWebDriver.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class HelloWebDriver {
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+
+public class HelloWebDriver {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("image");
+        searchInput.sendKeys(Keys.ENTER);
+        Thread.sleep(5000);
+        WebElement searchBtn = driver.findElement(By.xpath("//*[@id='hdtb-msb']/div[1]/div/div[2]/a"));
+        searchBtn.click();
+        Thread.sleep(5000);
+        List<WebElement> images = driver.findElements(By.tagName("img"));
+        if (!images.isEmpty()){
+            System.out.println("The tab contains images!");
+        }else{
+            System.out.println("No images on the tab!");
+        }
+        driver.quit();
+    }
 }
